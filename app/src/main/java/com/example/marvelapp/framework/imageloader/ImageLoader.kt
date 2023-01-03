3f7dabc6085b8d95562218dfd7ad38bd39308f0b
marcolino <pamarcolino@gmail.com>
2023-01-03 17:18:13 -0300

    Detalhes

---
+++ app/src/main/java/com/example/marvelapp/framework/imageloader/ImageLoader.kt
@@ -0,0 +1,10 @@
+package com.example.marvelapp.framework.imageloader
+
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+
+interface ImageLoader {
+
+    fun load(imageView: ImageView, imageUrl: String, @DrawableRes fallback: Int)
+
+}
---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -0,0 +1,70 @@
+package com.example.marvelapp.presentation.detail
+
+import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentDetailBinding
+import com.example.marvelapp.framework.imageloader.ImageLoader
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding get() = _binding!!
+
+    private val args by navArgs<DetailFragmentArgs>()
+
+    private val viewModel : DetailViewModel by viewModels()
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentDetailBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val detailViewArg = args.detailViewArg
+        binding.imageCharacter.run {
+            transitionName = detailViewArg.name
+            imageLoader.load(this, detailViewArg.imageUrl, R.drawable.ic_img_loading_error)
+        }
+
+        setSharedElementTransitionOnEnter()
+
+        viewModel.uiState.observe(viewLifecycleOwner) {
+
+        }
+
+        viewModel.getComics(detailViewArg.characterId)
+    }
+
+    private fun setSharedElementTransitionOnEnter() {
+        TransitionInflater.from(context).inflateTransition(android.R.transition.move).apply {
+            sharedElementEnterTransition = this
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
